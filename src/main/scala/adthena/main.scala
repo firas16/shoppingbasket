cee6b8200201b0d0723b7b7b0820dbafebf02735
firas16 <firasbnsassi@gmail.com>
2022-06-28 11:54:50 +0200

    add ci

--- src/main/scala/adthena/main.scala
+++ src/main/scala/adthena/main.scala
@@ -16,10 +16,10 @@ object shoppingBasket {
     val bread = new Good(Bread, 2)
     val soup = new Good(Soup, 2)
     println("----articles and prices-----")
-    println("apples price: "+ currency.toString+ apples.product.price )
-    println("Milk price: "+ currency.toString+ milk.product.price )
-    println("Bread price: "+ currency.toString+ bread.product.price )
-    println("Soup price: "+ currency.toString + soup.product.price )
+    println("apples price: " + currency.toString + apples.product.price)
+    println("Milk price: " + currency.toString + milk.product.price)
+    println("Bread price: " + currency.toString + bread.product.price)
+    println("Soup price: " + currency.toString + soup.product.price)
 
     //customer basket
     println("---------basket 1-------------")
@@ -33,8 +33,8 @@ object shoppingBasket {
     val discount = DiscountEngine.compute(basket, rule1)
 
     println("SubTotal: " + currency.toString + basket.price)
-    println(discount.map(offer => offer._1 + ": " + offer._2 ).mkString("\n"))
-    println("Total  price: ",currency.toString + (basket.price - discount.map(_._2).sum))
+    println(discount.map(offer => offer._1 + ": " + offer._2).mkString("\n"))
+    println("Total  price: ", currency.toString + (basket.price - discount.map(_._2).sum))
 
     println("---------basket 2-------------")
     // new basket
@@ -44,16 +44,16 @@ object shoppingBasket {
     val discount2 = DiscountEngine.compute(basket2, rule2)
 
     println("SubTotal: " + currency.toString + basket2.price)
-    println(discount2.map(offer => offer._1 + ": " + offer._2 ).mkString("\n"))
-    println("Total  price: "+ currency.toString+ (basket2.price - discount2.map(_._2).sum))
+    println(discount2.map(offer => offer._1 + ": " + offer._2).mkString("\n"))
+    println("Total  price: " + currency.toString + (basket2.price - discount2.map(_._2).sum))
 
     println("---------basket 3-------------")
     // calculate discount applying rule 1 and 2 on basket 3
     val basket3 = new Basket(List(apples, soup, milk, bread))
     val discount3 = DiscountEngine.compute(basket3, List(rule1, rule2))
     println("SubTotal: " + currency.toString + basket3.price)
-    println(discount3.map(offer => offer._1 + ": " + offer._2 ).mkString("\n"))
-    println("Total  price: "+ currency.toString+ (basket3.price - discount3.map(_._2).sum))
+    println(discount3.map(offer => offer._1 + ": " + offer._2).mkString("\n"))
+    println("Total  price: " + currency.toString + (basket3.price - discount3.map(_._2).sum))
   }
 }
 
